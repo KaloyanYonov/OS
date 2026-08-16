6343e9e34f92211832cdef93ff822156bec6b84a
Kaloyan Yonov <kaloqntyu@gmail.com>
2026-08-17 01:53:23 +0300

    finished : 2026-SE-01

--- C/Pipes-Processes/2026-SE-01/main.c
+++ C/Pipes-Processes/2026-SE-01/main.c
@@ -2,7 +2,8 @@
 #include <fcntl.h>
 #include <err.h>
 #include <stdint.h>
-
+#include <stdio.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[]){
 
@@ -28,23 +29,33 @@ int main(int argc, char* argv[]){
 		close(fd[0]);
 		
 		execlp("tar", "tar" ,"cf", "-" , dir, NULL);
+		err(1, "execlp");
 	}
 
 	close(fd[1]);
 	char buf[4096];
+	ssize_t n;
+	unsigned int hash = 0;
 
-	while(n = (read(fd[0], &buf, sizeof(buf))) >0 ){
+	while((n = read(fd[0], buf, sizeof(buf))) >0 ){
 
-		unsigned int hash = 0;
-		for(int i =0; i < n; ++i){
-				
-			has = hash ^ buf[i];
-
+		
+		for(int i =0; i < n; ++i){	
+			hash = hash ^ buf[i];
 		}
 
+	}
+
+	waitpid(pid, NULL,0);
+	char hexbuf[3];
+	int len = snprintf(hexbuf, sizeof(hexbuf), "%02x", hash);
+	
+	if(write(1, &hexbuf, len) == -1){
+		err(1, "write");
 	}
 
 	return 0;
 
 }
+
 
